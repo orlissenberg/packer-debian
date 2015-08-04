82383343c179192367b206697f3a1a0e1267e7c5
Onno Lissenberg <orlissenberg@gmail.com>
2015-08-04 14:43:06 +0200

    Fixed preseed config & shutdown command.

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-# remark: https://github.com/angstwad/docker.ubuntu/issues/43
-# apt-add-repository ppa:ansible/ansible
+#!/usr/bin/env bash
+
+apt-get install software-properties-common -y
+apt-get install sshpass -y
+apt-get update
 
 apt-get install -y python-dev python-apt python-pycurl python-pip python-virtualenv
 pip install paramiko PyYAML Jinja2 httplib2
