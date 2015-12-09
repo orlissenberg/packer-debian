27ea3e01c2f3e1fb947a016d24185e41597081a0
Onno Lissenberg <orlissenberg@gmail.com>
2015-12-09 02:01:18 +0100

    Might be needed after all.

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# apt-get install software-properties-common -y
-# apt-get install module-assistant -y
-# apt-get install linux-headers-`uname -r` -y
+apt-get install software-properties-common -y
+apt-get install module-assistant -y
+apt-get install linux-headers-`uname -r` -y
 
 apt-get update
